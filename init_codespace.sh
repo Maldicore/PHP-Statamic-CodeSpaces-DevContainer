dc213c84c7fef35b31212535be51465d47f204f0
yusuf <shunan@gmail.com>
2022-12-09 19:41:49 +0500

    initial

---
+++ init_codespace.sh
@@ -0,0 +1,19 @@
+cp .env.example .env
+
+composer install
+
+php please make:user
+
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan statamic:stache:warm
+php artisan queue:restart
+php artisan statamic:search:update --all
+php artisan statamic:static:clear
+php artisan statamic:static:warm --queue
+php artisan statamic:assets:generate-presets --queue
+
+npm install
+npm run dev 
+#npm run watch
